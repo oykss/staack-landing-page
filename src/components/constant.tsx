1208944e33411a7b876382eb38a8c3bc3ad62a61
Pavlo Chernichenko <pwned7067@gmail.com>
2025-03-12 13:56:49 +0200

    Added constant.tsx file with navigation elements

---
+++ src/components/constant.tsx
@@ -0,0 +1,32 @@
+import Logo from './ui/Logo/Logo';
+
+export const NAVIGATION_ITEMS = [
+  {
+    label: 'About',
+    path: '#',
+  },
+  {
+    label: 'Contact',
+    path: '#',
+  },
+  {
+    label: 'Join Waitlist!',
+    path: '#',
+  },
+  {
+    label: <Logo />,
+    path: '/',
+  },
+  {
+    label: 'Home',
+    path: '#',
+  },
+  {
+    label: 'Terms of Service',
+    path: '#',
+  },
+  {
+    label: 'Privacy Policy',
+    path: '#',
+  },
+];
