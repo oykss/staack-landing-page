ca04becd6ffa212884193231cbb7a37d4466215b
Pavlo Chernichenko <pwned7067@gmail.com>
2025-03-12 13:58:22 +0200

    Added main application with Header component and media request processing

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+import { useMediaQuery } from 'react-responsive';
+import Header from './components/Header/Header';
+import { useSetIsMediaPoints } from './store/mediaSlice/state';
+
+export default function App() {
+  const setIsMediaPoints = useSetIsMediaPoints();
+  const isMobile = useMediaQuery({ query: '(min-width: 768px)' });
+  const isTablet = useMediaQuery({ query: '(min-width: 1280px)' });
+
+  useEffect(() => {
+    setIsMediaPoints({ isMobile, isTablet });
+  }, [isMobile, isTablet, setIsMediaPoints]);
+
+  return (
+    <>
+      <Header />
+      <main></main>
+    </>
+  );
+}
