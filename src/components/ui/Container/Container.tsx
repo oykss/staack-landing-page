de4be66275255b1fbe98e808343c51b251bace65
Pavlo Chernichenko <pwned7067@gmail.com>
2025-03-14 11:19:07 +0200

    Type name changed

--- src/components/ui/Container/Container.tsx
+++ src/components/ui/Container/Container.tsx
@@ -2,11 +2,11 @@ import clsx from 'clsx';
 import { ReactNode } from 'react';
 import css from './Container.module.css';
 
-type TContainer = {
+type TContainerProps = {
   children: ReactNode;
   className?: string;
 };
 
-export default function Container({ children, className }: TContainer) {
+export default function Container({ children, className }: TContainerProps) {
   return <div className={clsx(css.container, className)}>{children}</div>;
 }
