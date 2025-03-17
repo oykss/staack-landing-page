82b45ef27c47738058cd460cd0450125dc20bb8e
Pavlo Chernichenko <pwned7067@gmail.com>
2025-03-17 22:11:00 +0200

    Add new components: About, Benefits, Download, Partners and Footer in the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import { useEffect } from 'react';
 import { useMediaQuery } from 'react-responsive';
+import About from './components/About/About';
+import Benefits from './components/Benefits/Benefits';
+import Download from './components/Download/Download';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
+import Partners from './components/Partners/Partners';
 import { useSetIsMediaPoints } from './store/mediaSlice/state';
 
 export default function App() {
@@ -18,7 +23,12 @@ export default function App() {
       <Header />
       <main>
         <Hero />
+        <Benefits />
+        <About />
+        <Download />
+        <Partners />
       </main>
+      <Footer />
     </>
   );
 }
