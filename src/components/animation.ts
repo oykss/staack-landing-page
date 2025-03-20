92f5451a5bf5fc26e664d5fafb3d8d942855d307
Pavlo Chernichenko <pwned7067@gmail.com>
2025-03-20 22:29:34 +0200

    Add animation effects for buttons when hovering, clicking and focusing

---
+++ src/components/animation.ts
@@ -0,0 +1,25 @@
+export const BTN_PRIMARY_EFFECT = {
+  whileHover: {
+    opacity: 0.8,
+    transition: {
+      duration: 0.2,
+    },
+  },
+  whileTap: {
+    y: 2,
+    opacity: 0.8,
+    transition: {
+      duration: 0.1,
+    },
+  },
+  whileFocus: {
+    opacity: 0.8,
+    transition: {
+      duration: 0.2,
+    },
+  },
+  initial: {
+    opacity: 1,
+    scale: 1,
+  },
+};
