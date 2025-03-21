e9b8b47144c562f3dabee0b64b8f2e3f1c96eeea
Pavlo Chernichenko <pwned7067@gmail.com>
2025-03-21 11:47:50 +0200

    Add animation for the partner list to improve visuals

--- src/components/Partners/PartnerList/PartnerList.tsx
+++ src/components/Partners/PartnerList/PartnerList.tsx
@@ -1,14 +1,27 @@
+import { motion } from 'motion/react';
 import { PARTNERS_LIST } from './constant';
 import css from './PartnerList.module.css';
 
 export default function PartnerList() {
   return (
-    <ul className={css.list}>
+    <motion.ul
+      className={css.list}
+      initial='hidden'
+      whileInView='visible'
+      viewport={{ once: true, amount: 0.2 }}
+    >
       {PARTNERS_LIST.map((partner, i) => (
-        <li key={i} className={css.item}>
+        <motion.li
+          key={i}
+          className={css.item}
+          variants={{
+            hidden: { x: -50, opacity: 0 },
+            visible: { x: 0, opacity: 1, transition: { delay: 0.15 * i } },
+          }}
+        >
           <img src={partner} alt={`Partner ${i}`} />
-        </li>
+        </motion.li>
       ))}
-    </ul>
+    </motion.ul>
   );
 }
