cc0334c45c34862e65beeac9d3858dc098240372
Pavlo Chernichenko <pwned7067@gmail.com>
2025-03-21 11:49:34 +0200

    Add animation for the logo using motion to enhance the visual effects

--- src/components/ui/Logo/Logo.tsx
+++ src/components/ui/Logo/Logo.tsx
@@ -1,13 +1,18 @@
 import clsx from 'clsx';
-
+import { motion } from 'motion/react';
+import { BTN_PRIMARY_EFFECT } from '../../animation';
 import GhostImg from '../GhostImg';
 import css from './Logo.module.css';
 
 export default function Logo({ className }: { className?: string }) {
   return (
-    <a href='/' className={clsx(css.logo, className)}>
+    <motion.a
+      href='/'
+      className={clsx(css.logo, className)}
+      {...BTN_PRIMARY_EFFECT}
+    >
       <GhostImg />
       <p>Staack</p>
-    </a>
+    </motion.a>
   );
 }
