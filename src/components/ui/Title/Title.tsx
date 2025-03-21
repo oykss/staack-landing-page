e3ee9612e63aea13043594f1863461c56014f3f4
Pavlo Chernichenko <pwned7067@gmail.com>
2025-03-21 11:46:54 +0200

    Refactor Title component animations for improved visibility and performance

--- src/components/ui/Title/Title.tsx
+++ src/components/ui/Title/Title.tsx
@@ -12,30 +12,27 @@ export default function Title({ title, subtitle, className }: TTitleProps) {
   return (
     <motion.div
       className={clsx(css.wrap, className)}
-      viewport={{ once: true, amount: 0.5 }}
+      initial='hidden'
+      whileInView='visible'
+      viewport={{ once: true, amount: 0.2 }}
     >
       {subtitle && (
         <motion.p
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1 }}
-          transition={{
-            duration: 0.4,
-            ease: 'easeOut',
-          }}
           className={css.subtitle}
+          variants={{
+            hidden: { opacity: 0 },
+            visible: { opacity: 1, transition: { duration: 0.2 } },
+          }}
         >
           {subtitle}
         </motion.p>
       )}
       <motion.h2
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1 }}
-        transition={{
-          delay: subtitle ? 0.2 : 0,
-          duration: 0.5,
-          ease: 'easeOut',
-        }}
         className={css.title}
+        variants={{
+          hidden: { opacity: 0 },
+          visible: { opacity: 1, transition: { delay: 0.2, duration: 0.3 } },
+        }}
       >
         {title}
       </motion.h2>
